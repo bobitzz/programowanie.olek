7db62237aa4087ed7b7e9f31d685338d3833c17a
olek <olek.h.kielce@gmail.com>
2022-09-08 11:40:29 +0200

    zadainie

--- Programowanie strukturalne i obiektowe/powtórzenie/powtórzenie.cpp
+++ Programowanie strukturalne i obiektowe/powtórzenie/powtórzenie.cpp
@@ -2,14 +2,36 @@
 
 using namespace std;
 
+void showMenu()
+{
+    system("cls");
+    cout << "Menu: \n";
+    cout << "1. Pole kwadratu \n";
+    cout << "2. Pole trojkata \n";
+}
+
+void chooseOptionFromUser()
+{
+    cout << "Wybierz opcje: \n";
+    int selectOption;
+    cin >> selectOption;
+}
+
+void doSelectedTask()
+{
+    if(selectOption == 1)
+}
+
 void mainProgram()
 {
     //1. wyświetlenie menu
+    showMenu();
 
     //2. wybranie opcji przez użytkownika
+    chooseOptionFromUser();
 
     //3. wykonanie wybranego zadania
-
+    doSelectedTask();
 }
 
 int main()
