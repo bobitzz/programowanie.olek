de935700d1f31cd4a57d8f0035545d0a37987713
olek <olek.h.kielce@gmail.com>
2022-09-08 11:01:47 +0200

    lekcja

---
+++ Programowanie strukturalne i obiektowe/powtórzenie/powtórzenie.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void mainProgram()
+{
+    //1. wyświetlenie menu
+
+    //2. wybranie opcji przez użytkownika
+
+    //3. wykonanie wybranego zadania
+
+}
+
+int main()
+{
+    mainProgram();
+    
+    //cout << "Hello World!\n";
+}
