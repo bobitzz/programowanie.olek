fb91e811603a20c8773a09a95cc537e128aae256
olek <olek.h.kielce@gmail.com>
2022-09-12 09:46:21 +0200

    ok

--- Programowanie strukturalne i obiektowe/powtórzenie/powtórzenie.cpp
+++ Programowanie strukturalne i obiektowe/powtórzenie/powtórzenie.cpp
@@ -4,47 +4,68 @@ using namespace std;
 
 void showMenu()
 {
-    system("cls");
-    cout << "Menu: \n";
-    cout << "1. Pole kwadratu \n";
-    cout << "2. Pole trojkata \n";
+	system("cls");
+	cout << "Menu: \n";
+	cout << "1. Pole kwadratu \n";
+	cout << "2. Pole trojkata \n";
 }
 
-void chooseOptionFromUser()
+int chooseOptionFromUser()
 {
-    cout << "Wybierz opcje: \n";
-    int selectOption;
-    cin >> selectOption;
+	cout << "Wybierz opcje: \n";
+	int selectedOption;
+	cin >> selectedOption;
+	return selectedOption;
 }
 
-void doSelectedTask()
+void SquareArea()
 {
-    if (selectOption == 1)
-    {
-        PoleKwadratu();
-    }
-    if (selectOption == 2)
-    {
-        PoleTrojkata();
-    }
+	float side, area;
+	cout << "podaj dlugosc boku: \n";
+	cin >> side;
+	area = side * side;
+	cout << "pole jest rowne: " << area << '\n';
+}
+
+void TriangleArea()
+{
+	float side, height, area;
+	cout << "podaj dlugosc boku: \n";
+	cin >> side;
+	cout << "Podaj wysokosc: ";
+	cin >> height;
+	area = (side * height) / 2;
+	cout << "pole jest rowne: " << area << '\n';
+}
+
+void doSelectedTask(int selectedOption)
+{
+	if (selectedOption == 1)
+	{
+		SquareArea();
+	}
+	if (selectedOption == 2)
+	{
+		TriangleArea();
+	}
 
 }
 
 void mainProgram()
 {
-    //1. wyświetlenie menu
-    showMenu();
+	//1. wyświetlenie menu
+	showMenu();
 
-    //2. wybranie opcji przez użytkownika
-    chooseOptionFromUser();
+	//2. wybranie opcji przez użytkownika
+	int selected = chooseOptionFromUser();
 
-    //3. wykonanie wybranego zadania
-    doSelectedTask();
+	//3. wykonanie wybranego zadania
+	doSelectedTask(selected);
 }
 
 int main()
 {
-    mainProgram();
-    
-    //cout << "Hello World!\n";
+	mainProgram();
+
+	//cout << "Hello World!\n";
 }
