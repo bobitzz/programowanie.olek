12eda8814bb49a0cc90af6b7f26623c4b27ba988
olek <olek.h.kielce@gmail.com>
2022-09-14 16:55:29 +0200

    da

--- Programowanie strukturalne i obiektowe/powtórzenie/powtórzenie.cpp
+++ Programowanie strukturalne i obiektowe/powtórzenie/powtórzenie.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -8,6 +9,11 @@ void showMenu()
 	cout << "Menu: \n";
 	cout << "1. Pole kwadratu \n";
 	cout << "2. Pole trojkata \n";
+	cout << "3. Liczby nieparzyste do 1 \n";
+	cout << "4. Ciag Fibonacciego \n";
+	cout << "5. Dzielnik podanej liczby \n";
+	cout << "6. Liczba na wyraz \n";
+	cout << "0. Zamknij program \n";
 }
 
 int chooseOptionFromUser()
@@ -38,6 +44,57 @@ void TriangleArea()
 	cout << "pole jest rowne: " << area << '\n';
 }
 
+void oddNubmer()
+{
+	int number;
+	cout << "podaj liczbe: \n";
+	cin >> number;
+
+	for (int i = number; i >= 1; i--)
+	{
+		if (i % 2 == 1)
+			cout << i << ", ";
+	}
+	cout << '\n';
+}
+
+void stringOfFibonnaciego()
+{
+	int liczba;
+	cout << "Podaj ile chcesz zobaczyc liczb z ciagu Fibonaciego: \n";
+	cin >> liczba;
+
+	int fibonaci_1 = 0, fibonaci_2 = 1;
+	for (int i = 0; i < liczba; i++)
+	{
+		cout << fibonaci_2 << ", ";
+		fibonaci_2 += fibonaci_1;
+		fibonaci_1 = fibonaci_2 - fibonaci_1;
+	}
+}
+
+void divisorOfNumber()
+{
+	int number;
+	cout << "podaj liczbe: \n";
+	cin >> number;
+
+	for (int i = 1; i <= number; i++)
+	{
+		if (number % i == 0)
+			cout << i << ", ";
+	}
+	cout << '\n';
+}
+
+void numberForWord()
+{
+	string slowa[10] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+	int number;
+	cout << "podaj liczbe do zapisania jej slownie: \n";
+	cin >> number;
+}
+
 void doSelectedTask(int selectedOption)
 {
 	switch (selectedOption)
@@ -50,6 +107,17 @@ void doSelectedTask(int selectedOption)
 	case 2:
 		TriangleArea();
 		break;
+	case 3:
+		oddNubmer();
+		break;
+	case 4:
+		stringOfFibonnaciego();
+		break;
+	case 5:
+		divisorOfNumber();
+		break;
+	case 6:
+		numberForWord();
 	default:
 		cout << "brak opcji w menu\n";
 		break;
@@ -80,6 +148,18 @@ void mainProgram()
 
 		//3. wykonanie wybranego zadania
 		doSelectedTask(selected);
+
+		//napisz funkcje, która wczyta liczbe od użytkownika i wyświetli wszystkie liczby nieparzyste od tej liczby do 1
+		oddNubmer();
+
+		//napisz program który wyświetli kolejną liczbę ciągu Fibonnaciego, ilość liczb wczytaj z klawiatury
+		stringOfFibonnaciego();
+
+		//napisz program który wyświwtli wszystkie dzilniki danej liczby
+		divisorOfNumber();
+
+		//napisz funkcje, która wczyta od użytkownika liczbe i wyświetli słownie poszczególne cyfry tej liczby
+		numberForWord();
 	} while (selected != 0);
 }
 
