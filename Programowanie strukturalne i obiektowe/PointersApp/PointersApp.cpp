f1d380288d5f64d776a7c2d1c12cbde8160b0ae0
olek <olek.h.kielce@gmail.com>
2022-10-06 11:36:42 +0200

    ok

--- Programowanie strukturalne i obiektowe/PointersApp/PointersApp.cpp
+++ Programowanie strukturalne i obiektowe/PointersApp/PointersApp.cpp
@@ -39,17 +39,62 @@ int main()
 	number = 77;
 	cout << number;
 
-	//deklaracja zmiennej  lokalnej o nazwie 'wsk' która jest wksaŸnikiem NA TYP int
+	//pozosta³y wolny obszar pamiêci ram
+
+	//deklaracja zmiennej  lokalnej o nazwie 'wsk' która jest wksaŸnikiem NA TYP int. Zmienna ta nie jest typu int
 	int* wsk;
 	/*
 	operator 'new' rezerwuje nowy obszar na stercie o wielkoœci typu int
+	i zwraca adres tego obszaru. Adres ten jest zapisywany do zmiennej lokalnej 'wsk'
 	*/
 	wsk = new int;
+	//zapisanie wartoœci 7 do obszaru pod adresem zapisanym we wskaŸniku 'wsk'
 	*wsk = 7;
 	//wyœwietlenie adresu pod jakim znajduje siê obszar na stercie
 	cout << wsk << "\n";
+	//wyœwietlanie zawartoœci obszaru pod adresem wskaŸnika 'wsk'
 	cout << *wsk << "\n";
 
 	//wyswietli 14
 	cout << *wsk * 2;
+	//zwalniamy obszar ze sterty
+	delete wsk;
+	wsk = NULL;
+
+	//*wsk = 55;
+	//std::cout << *wsk << "\n";
+
+	cout << "***********************************************\n";
+
+	short tab[3];
+
+	tab[0] = 5;
+	*(tab + 0) = 5;
+
+	tab[1] = 15;
+	*(tab + 1) = 15;
+
+	tab[2] = 15;
+	*(tab + 2) = 15;
+
+	cout << "Adres zerowego elementu tablicy \t" << tab << "\n";
+	cout << "Adres pierwszego elementu tablicy \t" << tab + 3 << "\n";
+
+	cout << "***********************************************\n";
+
+	int tab2[7];
+	const int rozmiar = 5;
+	//int tab3[number];
+	int tab4[rozmiar];
+
+	int* tab5 = new int[number];
+	tab5[7] = 15;
+	*(tab5 + 7) = 15;
+
+	delete tab5;
+
+	tab5 = new int[number];
+
+	delete tab5;
+
 }
