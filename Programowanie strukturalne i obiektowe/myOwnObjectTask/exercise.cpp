d6033508809c46150a210a9dcc097327d4a27d23
bobitzz <olek.h.kielce@gmailcom>
2022-10-22 15:08:19 +0200

    ok

--- Programowanie strukturalne i obiektowe/myOwnObjectTask/exercise.cpp
+++ Programowanie strukturalne i obiektowe/myOwnObjectTask/exercise.cpp
@@ -14,10 +14,18 @@ napisaæ klasê (oraz kod testuj¹cy) opisuj¹cy osobê.
 
 using namespace std;
 
+int yearNow()
+{
+	time_t now = time(0);
+	tm* ltm = new tm;
+	localtime_s(ltm, &now);
+	return 1900 + ltm->tm_year;
+}
+
 struct Information
 {
 	string name, surname, gender, city;
-	int yearOfBirthday, age, moreThan18;
+	int yearOfBirthday = yearNow();
 };
 
 class Person
@@ -31,6 +39,23 @@ public:
 		return 1900 + ltm->tm_year;
 	}
 
+	int moreThan18(bool adult)
+	{
+		if ((currentTime() - Personality.yearOfBirthday) > 18)
+			adult = true;
+		else
+			adult = false;
+
+		/*
+			if (adult == 1)
+				cout << "yes";
+			else if (adult == 0)
+				cout << "no";
+		*/
+
+		return adult;
+	}
+
 	Person(string name, string surname, string gender, string city, int yearOfBirthday)
 	{
 		this->Personality.name = name;
@@ -40,37 +65,31 @@ public:
 		this->Personality.yearOfBirthday = yearOfBirthday;
 	}
 
-	void ShowInfo()
+	void ShowInfo(bool adult)
 	{
 		cout << "****************\n";
-		cout << "name: " << Personality.name << '\n';
-		cout << "surname: " << Personality.surname << '\n';
-		cout << "gender: " << Personality.gender << '\n';
-		cout << "city: " << Personality.city << '\n';
-		cout << "year of birthday: " << Personality.yearOfBirthday << '\n';
-		cout << "age: " << (currentTime() - Personality.yearOfBirthday) << '\n';
-		//cout << "more than eighteen: " << moreThan18 << '\n';
+		cout << "Name: " << Personality.name << '\n';
+		cout << "Surname: " << Personality.surname << '\n';
+		cout << "Gender: " << Personality.gender << '\n';
+		cout << "City: " << Personality.city << '\n';
+		cout << "Year of birthday: " << Personality.yearOfBirthday << '\n';
+		cout << "Age: " << (currentTime() - Personality.yearOfBirthday) << '\n';
+		cout << "Adult: " << moreThan18(adult);
+		if (moreThan18(adult) == 1)
+			cout << ". Yes" << '\n';
+		else if (moreThan18(adult) == 0)
+			cout << ". No" << '\n';
 		cout << "****************\n";
-	}
-
-	int moreThan18()
-	{
-		if ((currentTime() - Personality.yearOfBirthday) > 18)
-			cout << "yes";
-		else
-			cout << "no";
 	}
 
 private:
 	Information Personality;
 };
 
-int main()
+int main(bool adult)
 {
 	setlocale(LC_CTYPE, "polish");
 
-
-
-	Person person1("Marek", "Wójcik", "Mê¿czyzna", "Kraków", 1998);
-	person1.ShowInfo();
+	Person person1("Marek", "Wójcik", "Mê¿czyzna", "Kraków", 1997);
+	person1.ShowInfo(adult);
 }
