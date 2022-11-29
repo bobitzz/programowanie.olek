5a4370c244e91f0b89ad98fb60e53d350687d6c9
olek <olek.h.kielce@gmail.com>
2022-11-29 09:50:06 +0100

    cxewac

--- Programowanie strukturalne i obiektowe/CollectionConsoleApp/TestListCollection.cs
+++ Programowanie strukturalne i obiektowe/CollectionConsoleApp/TestListCollection.cs
@@ -24,6 +24,10 @@ namespace CollectionConsoleApp
             }
             Console.WriteLine("\b");
 
+            //int MaxNumberFromInts = MaxFromInts(ListOfInts);
+            int MaxNumber = MaxFromAllTypes<int>(ListOfInts);
+            Console.WriteLine("Max from ints: " + MaxNumber);
+
             List<double> ListOfDouble = new List<double>();
             for (int i = 0; i < 5; i++)
             {
@@ -39,14 +43,37 @@ namespace CollectionConsoleApp
             }
             Console.WriteLine("\b");
 
-            int MaxNumber = MaxFromInts(ListOfInts);
-            Console.WriteLine("Max from ints: " + MaxNumber);
+            //double MaxNumberFromDoubles = MaxFromDoubles(ListOfDouble);
+            double MaxNumberFromDoubles = MaxFromAllTypes<double>(ListOfDouble);
+            Console.WriteLine("Max from doubles: " + MaxNumberFromDoubles);
         }
 
         private int MaxFromInts(List<int> list)
         {
             int MaxNumber = list[0];
             foreach (int element in list)
+            {
+                if (element > MaxNumber)
+                    MaxNumber = element;
+            }
+            return MaxNumber;
+        }
+
+        private double MaxFromDoubles(List<double> list)
+        {
+            double MaxFromDoubles = list[0];
+            foreach (double element in list)
+            {
+                if (element > MaxFromDoubles)
+                    MaxFromDoubles = element;
+            }
+            return MaxFromDoubles;
+        }
+
+        private T MaxFromAllTypes<T>(List<T> list)
+        {
+            T MaxNumber = list[0];
+            foreach (T element in list)
             {
                 if (element > MaxNumber)
                     MaxNumber = element;
