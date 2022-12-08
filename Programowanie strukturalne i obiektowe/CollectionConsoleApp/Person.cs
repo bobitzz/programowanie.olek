727cf98067f564c516486ab74f80e10387d1c904
olek <olek.h.kielce@gmail.com>
2022-12-08 11:40:22 +0100

    ceq

--- Programowanie strukturalne i obiektowe/CollectionConsoleApp/Person.cs
+++ Programowanie strukturalne i obiektowe/CollectionConsoleApp/Person.cs
@@ -11,17 +11,19 @@ namespace CollectionConsoleApp
         public string name;
         public string surname;
         public int age;
+        public int height;
 
-        public Person(string name, string surname, int age)
+        public Person(string name, string surname, int age, int height)
         {
             this.name = name;
             this.surname = surname;
             this.age = age;
+            this.height = height;
         }
 
         public void Info()
         {
-            Console.WriteLine(name + " " + surname + " " + age + " lat");
+            Console.WriteLine(name + " " + surname + " " + age + " lat, " + height + " cm");
         }
     }
 }
--- Programowanie strukturalne i obiektowe/CollectionConsoleApp/TestListOfObject.cs
+++ Programowanie strukturalne i obiektowe/CollectionConsoleApp/TestListOfObject.cs
@@ -12,18 +12,19 @@ namespace CollectionConsoleApp
         {
             List<Person> listOfPeople = new List<Person>();
 
-            Person person = new("Jan", "Kowalski", 40);
+            Person person = new("Jan", "Kowalski", 10, 170);
             listOfPeople.Add(person);
 
-            listOfPeople.Add(new Person("Ewa", "Nowak", 36));
-            listOfPeople.Add(new Person("Paweł", "Niezany", 15));
-            listOfPeople.Add(new Person("Bez", "Imienny", 14));
+            listOfPeople.Add(new Person("Ewa", "Nowak", 15, 150));
+            listOfPeople.Add(new Person("Paweł", "Niezany", 15, 120));
+            listOfPeople.Add(new Person("Bez", "Imienny", 14, 180));
 
             Console.WriteLine("Lista wszystkich osób");
             foreach (Person p in listOfPeople)
             {
                 p.Info();
             }
+            Console.WriteLine();
 
             List<Person> listOfPeoplePegi18;
 
@@ -34,6 +35,62 @@ namespace CollectionConsoleApp
                 p.Info();
             }
             Console.WriteLine();
+
+            /*
+            select *
+            form listOfPeople
+            order by age
+            */
+
+            List<Person> sortedListByAge = listOfPeople.OrderBy(x => x.age).ToList();
+            Console.WriteLine("Liczba osób według wieku");
+            foreach (Person p in sortedListByAge)
+            {
+                p.Info();
+            }
+            Console.WriteLine();
+
+            List<Person> sortedListByAge2 = listOfPeople.OrderByDescending(x => x.age)
+                .ThenBy(x => x.height)
+                .ToList();
+            Console.WriteLine("Liczba osób według wieku malejąco i wzrostu");
+            foreach (Person p in sortedListByAge2)
+            {
+                p.Info();
+            }
+            Console.WriteLine();
+
+            List<Person> sortedListByHeight = listOfPeople.OrderBy(x => x.height).ToList();
+            Console.WriteLine("Liczba osób według wzrostu");
+            foreach (Person p in sortedListByHeight)
+            {
+                p.Info();
+            }
+            Console.WriteLine();
+
+            List<Person> sortedListByName = listOfPeople.OrderBy(x => x.name).ToList();
+            Console.WriteLine("Liczba osób według imienia");
+            foreach (Person p in sortedListByName)
+            {
+                p.Info();
+            }
+            Console.WriteLine();
+
+            /*
+            List<Person> sortedListByNameNextLetter = listOfPeople.OrderBy(x => x.name).ToList();
+            Console.WriteLine("Liczba osób według drugiej litery imienia");
+            foreach (Person p in sortedListByNameNextLetter)
+            {
+                p.Info();
+            }
+            Console.WriteLine();
+            */
+
+            int minAge = listOfPeople.Min(x => x.age);
+            Console.WriteLine("Minimalny wzrost to: " + minAge);
+
+            bool isLegalAge = listOfPeople.All(x => x.age < 18);
+            Console.WriteLine("Czy wszystkie osoby są niepełnoletnie: " + isLegalAge);
         }
     }
 }
