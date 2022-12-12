c78a5f2b40061a3adf77e35e3ee57bff50eb676b
olek <olek.h.kielce@gmail.com>
2022-12-12 15:13:42 +0100

    ok

---
+++ Programowanie strukturalne i obiektowe/ShapeConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ShapeConsoleApp.Shapes;
+
+namespace ShapeConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new();
+            rectangle.ShowInfo();
+
+            Rectangle secondRectangle = new Rectangle(54, 99);
+            secondRectangle.ShowInfo();
+        }
+    }
+}
---
+++ Programowanie strukturalne i obiektowe/ShapeConsoleApp/Shapes/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeConsoleApp.Shapes
+{
+    class Rectangle
+    {
+        private double sideA;
+        private double sideB;
+
+        public Rectangle()
+        {
+            sideA = 21;
+            sideB = 37;
+        }
+
+        public Rectangle(double sideA, double sideB)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+        }
+
+        double Area()
+        {
+            return sideA * sideB;
+        }
+
+        public double Perimeter()
+        {
+            return 2 * sideA + 2 * sideB;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Informacja o prostokącie");
+            Console.WriteLine("Bok a = " + sideA);
+            Console.WriteLine("Bok b = " + sideB);
+            Console.WriteLine("Pole " + Area());
+            Console.WriteLine("Obwód " + Perimeter());
+        }
+    }
+}
