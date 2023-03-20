d53dbb15b02b0ef9d3f2a15500255a5ac4302f83
olek <olek.h.kielce@gmail.com>
2023-03-20 10:39:31 +0100

    database

---
+++ Programowanie strukturalne i obiektowe/SchoolApp/Database/SchoolDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolApp.Database.Entities;
+
+namespace SchoolApp.Database
+{
+    class SchoolDatabase
+    {
+        public List<SchoolClass> SchoolClasses { get; set; } = new List<SchoolClass>();
+    }
+}
