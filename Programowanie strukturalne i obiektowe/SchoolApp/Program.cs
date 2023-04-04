939fe2a837cf895c8a7a9313ae618d672491f3c3
olek <olek.h.kielce@gmail.com>
2023-04-04 09:52:30 +0200

    baza

--- Programowanie strukturalne i obiektowe/SchoolApp/Program.cs
+++ Programowanie strukturalne i obiektowe/SchoolApp/Program.cs
@@ -11,65 +11,8 @@ namespace SchoolApp
     {
         static void Main(string[] args)
         {
-            SchoolDatabase schoolDatabase = new SchoolDatabase();
-
-            SchoolClass schoolClass = new SchoolClass();
-            schoolClass.Id = 1;
-            schoolClass.Name = "1A";
-
-            schoolDatabase.SchoolClasses.Add(schoolClass);
-
-            schoolClass = new SchoolClass() {
-                Id = 2,
-                Name = "1B"
-            };
-            schoolDatabase.SchoolClasses.Add(schoolClass);
-
-            schoolClass = new SchoolClass() {
-                Id = 3,
-                Name = "1C"
-            };
-            schoolDatabase.SchoolClasses.Add(schoolClass);
-
-            schoolClass = new SchoolClass() {
-                Id = 4,
-                Name = "2C"
-            };
-            schoolDatabase.SchoolClasses.Add(schoolClass);
-
-            Console.WriteLine("Lista klas w szkole:");
-            foreach (var item in schoolDatabase.SchoolClasses)
-            {
-                Console.WriteLine(item.Id + " " + item.Name);
-            }
-
-            /*
-             select *
-             from SchoolClasses
-             where Nazwa like '%C'
-            */
-            List<SchoolClass> classWithC = new List<SchoolClass>();
-            foreach (SchoolClass item in schoolDatabase.SchoolClasses)
-            {
-                if (item.Name.EndsWith("C"))
-                    classWithC.Add(item);
-            }
-
-            Console.WriteLine("Klasy kończące się na literę 'C'");
-            foreach (var item in classWithC)
-            {
-                Console.WriteLine(item.Id + " " + item.Name);
-            }
-
-            classWithC = schoolDatabase
-                .SchoolClasses
-                .Where((SchoolClass sc) => /*return */sc.Name.EndsWith("C"))
-                .ToList();
-            Console.WriteLine("Klasy kończące się na literę 'C' uzyskane za pomocą metody LINQ");
-            foreach (var item in classWithC)
-            {
-                Console.WriteLine(item.Id + " " + item.Name);
-            }
+            School school = new();
+            school.Run();
         }
     }
 }
---
+++ Programowanie strukturalne i obiektowe/SchoolApp/School.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolApp.Database;
+
+namespace SchoolApp
+{
+    class School
+    {
+        private SchoolDatabase schoolDatabase = new SchoolDatabase();
+
+        public void Run()
+        {
+            while (true)
+            {
+                ShowMenu();
+
+                Console.WriteLine("Wybierz: ");
+                //int option = int.Parse(Console.ReadLine());
+                if (!int.TryParse(Console.ReadLine(), out int option))
+                    continue;
+
+                switch(option)
+                {
+                    case 1:
+                        //uzupełnić metodę
+                        break;
+                    case 2:
+                        //uzupełnić metodę
+                        break;
+                    case 3:
+                        //uzupełnić metodę
+                        break;
+                    case 4:
+                        //uzupełnić metodę
+                        break;
+
+                    case 0:
+                        return;
+                }
+            }
+        }
+
+        public void ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Lista dostępnych opcji:");  //CRUD
+            Console.WriteLine("1. Dodanie nowej klasy");  //C - create
+            Console.WriteLine("2. Usunięcie klasy");  //D - delete
+            Console.WriteLine("3. Modyfikacja klasy");  //U - update
+            Console.WriteLine("4. Wyświetlenie wszytkich klas");  //R - read
+            
+            Console.WriteLine("0. Koniec programu");
+        }
+    }
+}
