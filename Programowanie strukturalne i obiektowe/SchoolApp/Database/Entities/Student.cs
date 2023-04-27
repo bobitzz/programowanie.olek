ed32482612d092b61842df2e39491886538a5f08
olek <olek.h.kielce@gmail.com>
2023-04-27 11:25:54 +0200

    update student column

--- Programowanie strukturalne i obiektowe/SchoolApp/Database/Entities/Student.cs
+++ Programowanie strukturalne i obiektowe/SchoolApp/Database/Entities/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace SchoolApp.Database.Entities
 {
     class Student
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Surname { get; set; }
 
         public int SchoolClassId { get; set; }
