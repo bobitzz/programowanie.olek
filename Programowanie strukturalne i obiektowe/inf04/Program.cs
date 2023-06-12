e46e7ecc24ba835634f0ac9d9770540c0464ca7e
programsta <stanowisko14>
2023-06-12 09:14:30 +0200

    exam

---
+++ Programowanie strukturalne i obiektowe/inf04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace inf04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj a");
+            uint numberA = uint.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj b");
+            uint numberB = uint.Parse(Console.ReadLine());
+
+            Console.WriteLine("NWD: " + NWD(numberA, numberB));
+        }
+
+        static uint NWD(uint numberA, uint numberB)
+        {
+            if (numberA != numberB)
+            {
+                do
+                {
+                    if (numberA > numberB)
+                        numberA -= numberB;
+                    else
+                        numberB -= numberA;
+                } while (numberA != numberB);
+                return numberA;
+            }
+            else
+                return numberA;
+        }
+    }
+}
