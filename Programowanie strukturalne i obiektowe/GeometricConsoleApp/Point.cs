6c43524a43502df5faeb106e8c5a845adb49a4a3
Test <test@test.pl>
2023-11-03 15:14:25 +0100

    lekcja

---
+++ Programowanie strukturalne i obiektowe/GeometricConsoleApp/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricConsoleApp
+{
+    public class Point
+    {
+        private
+            int x, y;
+
+        public void Display()
+        {
+            Console.WriteLine(x.ToString() + ", " + y.ToString());
+        }
+    }
+}
---
+++ Programowanie strukturalne i obiektowe/GeometricConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using GeometricConsoleApp;
+
+Point p = new Point();
+p.x = 5;
+p.y=7;
+p.Display();
+
+Point p2 = new Point();
+p2.x = 25;
+p2.y = 27;
+p2.Display();
