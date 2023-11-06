3fc8334eea2cf8c5cb489034814ab6ea24e77056
Test <test@test.pl>
2023-11-06 09:54:55 +0100

    lekcja

--- Programowanie strukturalne i obiektowe/GeometricConsoleApp/Point.cs
+++ Programowanie strukturalne i obiektowe/GeometricConsoleApp/Point.cs
@@ -8,12 +8,40 @@ namespace GeometricConsoleApp
 {
     public class Point
     {
-        private
-            int x, y;
+        private int x, y;
 
         public void Display()
         {
             Console.WriteLine(x.ToString() + ", " + y.ToString());
+        }
+
+        //w jezyku c++
+        public void SetX(int a)
+        {
+            if (a > 0)
+                x = a;
+            else
+                throw new ArgumentException("Nie mozna ustawic wartosci ujemnych");
+        }
+
+        public int GetX()
+        {
+            return x;
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                if (value > 0)
+                    y = value;
+                else
+                    throw new ArgumentException("Nie mozna ustawic wartosci ujemnych");
+            }
         }
     }
 }
--- Programowanie strukturalne i obiektowe/GeometricConsoleApp/Program.cs
+++ Programowanie strukturalne i obiektowe/GeometricConsoleApp/Program.cs
@@ -1,11 +1,15 @@
 ﻿using GeometricConsoleApp;
 
 Point p = new Point();
-p.x = 5;
-p.y=7;
+//p.x = 5;
+p.SetX(5);
+//p.y = 7;
+p.Y = 7;
 p.Display();
 
 Point p2 = new Point();
-p2.x = 25;
-p2.y = 27;
+//p2.x = 25;
+p2.SetX(25);
+//p2.y = 27;
+p2.Y = 27;
 p2.Display();
