3d07d804c31df3d65cd7665028efb2fc7f31018e
Test <test@test.pl>
2023-12-07 09:41:00 +0100

    koniec lekcji

--- Programowanie strukturalne i obiektowe/WinFormsAppFirst/MainForm.Designer.cs
+++ Programowanie strukturalne i obiektowe/WinFormsAppFirst/MainForm.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            HelloButton = new Button();
+            textBoxName = new TextBox();
+            laeblName = new Label();
+            SuspendLayout();
+            // 
+            // HelloButton
+            // 
+            HelloButton.Location = new Point(182, 146);
+            HelloButton.Name = "HelloButton";
+            HelloButton.Size = new Size(146, 70);
+            HelloButton.TabIndex = 0;
+            HelloButton.Text = "Hello";
+            HelloButton.UseVisualStyleBackColor = true;
+            HelloButton.Click += button1_Click;
+            // 
+            // textBoxName
+            // 
+            textBoxName.Location = new Point(182, 117);
+            textBoxName.Name = "textBoxName";
+            textBoxName.Size = new Size(276, 23);
+            textBoxName.TabIndex = 1;
+            textBoxName.Text = "Jan";
+            // 
+            // laeblName
+            // 
+            laeblName.AutoSize = true;
+            laeblName.Font = new Font("Miriam Libre", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            laeblName.Location = new Point(182, 90);
+            laeblName.Name = "laeblName";
+            laeblName.Size = new Size(181, 24);
+            laeblName.TabIndex = 2;
+            laeblName.Text = "What`s your name?";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(laeblName);
+            Controls.Add(textBoxName);
+            Controls.Add(HelloButton);
+            Name = "MainForm";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button HelloButton;
+        private TextBox textBoxName;
+        private Label laeblName;
     }
 }
--- Programowanie strukturalne i obiektowe/WinFormsAppFirst/MainForm.cs
+++ Programowanie strukturalne i obiektowe/WinFormsAppFirst/MainForm.cs
@@ -9,7 +9,12 @@ namespace WinFormsAppFirst
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            string name = textBoxName.Text;
+            MessageBox.Show($"Witaj {name} w pierwszym programie",
+                "Powitanie",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.None,
+                MessageBoxDefaultButton.Button2);
         }
     }
 }
