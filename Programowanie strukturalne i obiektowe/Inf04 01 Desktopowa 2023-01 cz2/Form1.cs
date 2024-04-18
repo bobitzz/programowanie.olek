b40d430b3de069d3ef051155e971ec8bd437a37e
Uczeń <Ucze@213-13>
2024-04-18 09:21:02 +0200

    koneic egzaminu

--- Programowanie strukturalne i obiektowe/Inf04 01 Desktopowa 2023-01 cz2/Form1.cs
+++ Programowanie strukturalne i obiektowe/Inf04 01 Desktopowa 2023-01 cz2/Form1.cs
@@ -2,6 +2,7 @@ namespace Inf04_01_Desktopowa_2023_01_cz2
 {
     public partial class FormAddEmployee : Form
     {
+        //pola z ma³ych liter
         string Password = "";
         public FormAddEmployee()
         {
@@ -11,30 +12,24 @@ namespace Inf04_01_Desktopowa_2023_01_cz2
         private void buttonPasswordGenerator_Click(object sender, EventArgs e)
         {
             int PasswordLength = int.Parse(textBoxPassowrdGenerator.Text);
-            //string[] PasswordBigLettrs = { "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S",
-            //    "D", "", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M" };
+            string[] PasswordBigLettrs = { "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S",
+                "D", "", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M" };
             string[] PasswordSmallLettrs = {"q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s",
                 "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"};
-            int[] PasswordNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            int[] PasswordNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }; //zmnienic na string
             string[] PasswordSpecialSigns = { "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=" };
 
             Random rnd = new();
             if (checkBoxPassGenSmallAndBigSigns.Checked)
             {
-                //Password += PasswordBigLettrs[rnd.Next(PasswordBigLettrs.Length)];
                 Password += PasswordSmallLettrs[rnd.Next(PasswordSmallLettrs.Length)].ToUpper();
             }
             if (checkBoxPassGenNumbers.Checked)
                 Password += PasswordNumbers[rnd.Next(PasswordNumbers.Length)];
             if (checkBoxPassGenSpecialSigns.Checked)
                 Password += PasswordSpecialSigns[rnd.Next(PasswordSpecialSigns.Length)];
-            if (Password.Length < PasswordLength)
-            {
-                while (Password.Length < PasswordLength)
-                {
-                    Password += PasswordSmallLettrs[rnd.Next(PasswordSmallLettrs.Length)];
-                }
-            }
+            while (Password.Length < PasswordLength)
+                Password += PasswordSmallLettrs[rnd.Next(PasswordSmallLettrs.Length)];
             MessageBox.Show(Password);
         }
 
