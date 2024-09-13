82fcf638bf6854b54503ad2300dd86f8fa8dd9f4
Uczen <Uczen@DESKTOP-6O2JQB3>
2024-09-13 15:00:07 +0200

    calculator

---
+++ Programowanie strukturalne i obiektowe/SimpleCalculatorMauiApp/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿namespace SimpleCalculatorMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string strFirstNumber = firstNumberEntry.Text;
+
+            int firstNumber;
+            if (!int.TryParse(strFirstNumber, out firstNumber))
+            {
+                //komunikat o błędzie
+                resultLabel.Text = "Niepoprawna pierwsza liczba";
+                resultLabel.TextColor = Colors.Red;
+                return;
+            }
+
+            if(!int.TryParse(secondNumberEntry.Text, out int secondNumber))
+            {
+                //komunikat o błędzie
+                resultLabel.Text = "Niepoprawna druga liczba";
+                resultLabel.TextColor = Colors.Red;
+                return;
+            }
+
+            int result = 0;
+            checked
+            {
+                if (operationAddRadioButton.IsChecked)
+                {
+                    result = firstNumber + secondNumber;
+                }
+                else if (operationSubRadioButton.IsChecked)
+                {
+                    result = firstNumber - secondNumber;
+                }
+                else if (operationMulRadioButton.IsChecked)
+                {
+                    result = firstNumber * secondNumber;
+                }
+                else if (operationDivRadioButton.IsChecked)
+                {
+                    result = firstNumber / secondNumber;
+                }
+            }
+
+            resultLabel.Text = $"Wynik operacji {result}";
+            resultLabel.TextColor = Colors.DarkGrey;
+        }
+    }
+}
