855b2e72eca43b0927d1d0d3ffd7b4829a231941
Uczen <Uczen@DESKTOP-6O2JQB3>
2024-09-17 14:24:35 +0200

    next lesson

---
+++ Programowanie strukturalne i obiektowe/BindingMauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BindingMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Programowanie strukturalne i obiektowe/BindingMauiApp/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace BindingMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            rotationLabel.Rotation = rotationSlider.Value;
+        }
+    }
+}
