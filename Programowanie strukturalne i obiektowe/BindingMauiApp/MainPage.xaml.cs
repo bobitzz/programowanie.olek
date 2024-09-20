c934a7430cae943e05fa4821f7ac5f893885e4f7
Uczen <Uczen@DESKTOP-6O2JQB3>
2024-09-20 15:29:14 +0200

    asdasd

--- Programowanie strukturalne i obiektowe/BindingMauiApp/MainPage.xaml.cs
+++ Programowanie strukturalne i obiektowe/BindingMauiApp/MainPage.xaml.cs
@@ -2,6 +2,13 @@
 {
     public partial class MainPage : ContentPage
     {
+        public string Message { get; set; }
+        private string returnMessage {  get; set; }
+        public string ReturnMessage {
+            get { return returnMessage; }
+            set { return returnMessage; }
+        }
+
         public MainPage()
         {
             InitializeComponent();
@@ -11,5 +18,13 @@
         {
             rotationLabel.Rotation = rotationSlider.Value;
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            ReturnMessage = "Wartość z entry " + Message;
+            //OnPropertyChanged(ReturnMessage);
+            //OnPropertyChanged(nameof(ReturnMessage));
+        }
     }
+
 }
