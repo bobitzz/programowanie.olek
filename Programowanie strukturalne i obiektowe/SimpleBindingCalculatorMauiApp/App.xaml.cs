0d5e7b47b3b1c076a87551dc18139a22a4b1ce06
Uczen <Uczen@DESKTOP-6O2JQB3>
2024-09-24 14:23:42 +0200

    next

---
+++ Programowanie strukturalne i obiektowe/SimpleBindingCalculatorMauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleBindingCalculatorMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Programowanie strukturalne i obiektowe/SimpleBindingCalculatorMauiApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace SimpleBindingCalculatorMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public string firstNumber {  get; set; }
+        public string SecondNumber {  get; set; }
+        public string IsOperationAdd {  get; set; }
+        public string IsOperationSub {  get; set; }
+        public string IsOperationMul {  get; set; }
+        public string IsOperationDiv {  get; set; }
+
+        private string resultMessage;
+
+        public string ResultMessage
+        {
+            get { return resultMessage; }
+            set { resultMessage = value; OnPropertyChanged(); }
+    }
+
+}
