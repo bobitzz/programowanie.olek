14538cd9b93bdf264ba728870fd6227336594243
Uczeń <Ucze@DESKTOP-RGP55N4>
2024-10-04 14:24:23 +0200

    lol

--- Programowanie strukturalne i obiektowe/UnitsCalculatorMauiApp/MainPage.xaml.cs
+++ Programowanie strukturalne i obiektowe/UnitsCalculatorMauiApp/MainPage.xaml.cs
@@ -38,11 +38,58 @@ namespace UnitsCalculatorMauiApp
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            
-        }
-        public void MilimettersToCentimeter()
-        {
+            double.TryParse(UnitToChange, out double unitToChange);
 
+            switch(SelectedUnitToChange)
+            {
+                case "mm":
+                    showResult(MilimettersToCentimeter(unitToChange, SelectedUnit));
+                    break;
+                case "cm":
+                    showResult(MilimettersToCentimeter(unitToChange, SelectedUnit) / 10);
+                    break;
+                case "dm":
+                    showResult(MilimettersToCentimeter(unitToChange, SelectedUnit) / 100);
+                    break;
+                case "m":
+                    showResult(MilimettersToCentimeter(unitToChange, SelectedUnit) / 1000);
+                    break;
+                case "km":
+                    showResult(MilimettersToCentimeter(unitToChange, SelectedUnit) / 1000000);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private double MilimettersToCentimeter(double value, string unit)
+        {
+            switch(unit)
+            {
+                case "mm":
+                    return value;
+                    break;
+                case "cm":
+                    value = value * 10;
+                    break;
+                case "dm":
+                    value = value * 100;
+                    break;
+                case "m":
+                    value = value * 1000;
+                    break;
+                case "km":
+                    value = value * 1000000;
+                    break;
+                default:
+                    break;
+            }
+
+            return value;
+        }
+        private void showResult(double value)
+        {
+            UnitCalculatorResult = "Konwersja jest rowna: " + value;
         }
     }
 
