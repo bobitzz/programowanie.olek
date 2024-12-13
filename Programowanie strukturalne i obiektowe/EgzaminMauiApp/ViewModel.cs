db73df3ce0caa11f5b1ce072eafb9a67c4e42aa2
Uczeń <Ucze@DESKTOP-RGP55N4>
2024-12-13 13:14:11 +0100

    1312

--- Programowanie strukturalne i obiektowe/EgzaminMauiApp/ViewModel.cs
+++ Programowanie strukturalne i obiektowe/EgzaminMauiApp/ViewModel.cs
@@ -26,7 +26,6 @@ namespace EgzaminMauiApp
         }
 
         private string die1;
-
         public string Die1
         {
             get { return die1; }
@@ -34,7 +33,6 @@ namespace EgzaminMauiApp
         }
 
         private string die2;
-
         public string Die2
         {
             get { return die2; }
@@ -42,7 +40,6 @@ namespace EgzaminMauiApp
         }
 
         private string die3;
-
         public string Die3
         {
             get { return die3; }
@@ -50,7 +47,6 @@ namespace EgzaminMauiApp
         }
 
         private string die4;
-
         public string Die4
         {
             get { return die4; }
@@ -58,73 +54,71 @@ namespace EgzaminMauiApp
         }
 
         private string die5;
-
         public string Die5
         {
             get { return die5; }
             set { die5 = value; OnPropertyChanged(); }
         }
 
-        private Command rollCommand;
-
-        public Command RollCommand
-        {
-            get { return rollCommand; }
-            set { rollCommand = value; }
-        }
-
         private Command resetGameCommand;
-
         public Command ResetGameCommand
         {
             get { return resetGameCommand; }
             set { resetGameCommand = value; }
         }
+
+        private Command rollDiceCommand;
+        public Command RollDiceCommand
+        {
+            get { return rollDiceCommand; }
+            set { rollDiceCommand = value; }
+        }
         #endregion
 
         private string[] dieceImages =
         {
-        "k1.jpg",
-        "k2.jpg", 
-        "k3.jpg",
-        "k4.jpg",
-        "k5.jpg",
-        "k6.jpg",
-    };
+            "k1.jpg",
+            "k2.jpg",
+            "k3.jpg",
+            "k4.jpg",
+            "k5.jpg",
+            "k6.jpg",
+        };
 
         public ViewModel()
         {
-            RollCommand = new Command(RollDice);
+            RollDiceCommand = new Command(RollDice);
             ResetGameCommand = new Command(ResetGame);
-            Die1 = "question.jpg";
-            Die2 = "question.jpg";
-            Die3 = "question.jpg";
-            Die4 = "question.jpg";
-            Die5 = "question.jpg";
+            ResetGameCommand.Execute(this);
         }
 
-        int totalScore = 0;
-
+        private int totalGameScore = 0;
+        
         public void RollDice()
         {
             Random random = new Random();
-            Die1 = dieceImages[random.Next(0, 5)];
-            Die2 = dieceImages[random.Next(0, 5)];
-            Die3 = dieceImages[random.Next(0, 5)];
-            Die4 = dieceImages[random.Next(0, 5)];
-            Die5 = dieceImages[random.Next(0, 5)];
 
-            int rollResult = 5;
-            rollResult += Array.IndexOf(dieceImages, Die1);
-            rollResult += Array.IndexOf(dieceImages, Die2);
-            rollResult += Array.IndexOf(dieceImages, Die3);
-            rollResult += Array.IndexOf(dieceImages, Die4);
-            rollResult += Array.IndexOf(dieceImages, Die5);
+            int[] dieceValues = new int[5]
+            {
+                random.Next(1, 7),
+                random.Next(1, 7),
+                random.Next(1, 7),
+                random.Next(1, 7),
+                random.Next(1, 7)
+            };
 
-            totalScore += rollResult;
+            Die1 = dieceImages[dieceValues[0] - 1];
+            Die2 = dieceImages[dieceValues[1] - 1];
+            Die3 = dieceImages[dieceValues[2] - 1];
+            Die4 = dieceImages[dieceValues[3] - 1];
+            Die5 = dieceImages[dieceValues[4] - 1];
 
-            CurrentRollResult = $"Wynik tego losowania: {rollResult}";
-            TotalGameResult = $"Wynik gry: {totalScore}";
+            var duplicateValues = dieceValues.GroupBy(x => x).Where(g => g.Count() > 1).SelectMany(g => g);
+            int currentRollTotal = duplicateValues.Sum();
+
+            totalGameScore += currentRollTotal;
+            CurrentRollResult = $"Wynik tego losowania {currentRollTotal}";
+            TotalGameResult = $"Wynik całej gry {totalGameScore}";
         }
 
         public void ResetGame()
@@ -134,9 +128,9 @@ namespace EgzaminMauiApp
             Die3 = "question.jpg";
             Die4 = "question.jpg";
             Die5 = "question.jpg";
-            CurrentRollResult = "Wynik tego losowania: 0";
-            totalScore = 0;
-            TotalGameResult = "Wynik gry: 0";
+            CurrentRollResult = $"Wynik tego losowania 0";
+            totalGameScore = 0;
+            TotalGameResult = $"Wynik całej gry 0";
         }
     }
 }
