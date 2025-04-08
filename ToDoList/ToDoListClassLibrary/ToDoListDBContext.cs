9a3c9a1863dfc2e08c81fda1cf94c5c32aac4d36
Uczeń <Ucze@DESKTOP-RGP55N4>
2025-04-08 13:49:35 +0200

    ToDoList

---
+++ ToDoList/ToDoListClassLibrary/ToDoListDBContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+using ToDoListClassLibrary.Models;
+
+namespace ToDoListClassLibrary;
+
+public partial class ToDoListDBContext : DbContext
+{
+    public ToDoListDBContext()
+    {
+    }
+
+    public ToDoListDBContext(DbContextOptions<ToDoListDBContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<List> Lists { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=localhost;database=todolist4c;user=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.4.32-mariadb"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_general_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<List>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("list");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11)")
+                .HasColumnName("id");
+            entity.Property(e => e.IsDone).HasColumnName("isDone");
+            entity.Property(e => e.Text)
+                .HasMaxLength(50)
+                .HasColumnName("text");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ToDoList/ToDoListClassLibrary/ToDoListRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Tar;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoListClassLibrary.Models;
+
+namespace ToDoListClassLibrary
+{
+    public class ToDoListRepository
+    {
+        private ToDoListDBContext dbContext;
+
+        public ToDoListRepository()
+        {
+            dbContext = new ToDoListDBContext(); ;
+        }
+
+        public void AddNewElementToTheList(string text)
+        {
+            List list = new()
+            {
+                Text = text
+            };
+            dbContext.Lists.Add(list);
+            dbContext.SaveChanges();
+        }
+    }
+}
