9bf0413a3e405908fc6bb5abbd67b28d5e1c702a
Sala213 <Sala213@ezse.pl>
2025-09-18 15:27:34 +0200

    asd

---
+++ Programowanie strukturalne i obiektowe/ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ile wygenerować losowań?");
+int losowanie = int.Parse(Console.ReadLine());
+Console.WriteLine("Zestaw wylosowanych liczb:");
+Random random = new Random();
+
+for(int i = 1; i <= losowanie; i++)
+{
+    Console.WriteLine("Losowanie " + i + ":");
+    for(int j = 0; j < 6; j++)
+        Console.Write(random.Next(1, 49) + ", ");
+}
