5aa42703cac1236422f96eb163251931a0a836a9
Sala213 <Sala213@ezse.pl>
2025-10-23 15:18:30 +0200

    egz 1

---
+++ Programowanie strukturalne i obiektowe/ConsoleAppCezar/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Podaj tekst");
+string text = Console.ReadLine();
+Console.WriteLine("Podaj klucz");
+int key = int.Parse(Console.ReadLine());
+Console.WriteLine(SzyfrCezara(text, key));
+
+static string SzyfrCezara(string text, int key)
+{
+    string result = "";
+    for (int i = 0; i < text.Length; i++)
+    {
+        char letter = text[i];
+
+        if (letter != 32)
+        {
+            result += (char)((letter + key) % 256);
+        }
+        else
+        {
+            result += (char)(letter % 256);
+        }
+    }
+    return result;
+}
