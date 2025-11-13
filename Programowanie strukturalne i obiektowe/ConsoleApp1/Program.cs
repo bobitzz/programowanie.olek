7ff49e5d60b8cdf7932dcbcac7dbc168fdc4410a
Sala213 <Sala213@ezse.pl>
2025-11-13 15:19:52 +0100

    asd

--- Programowanie strukturalne i obiektowe/ConsoleApp1/Program.cs
+++ Programowanie strukturalne i obiektowe/ConsoleApp1/Program.cs
@@ -1,11 +1,30 @@
-﻿Console.WriteLine("Ile wygenerować losowań?");
+﻿using System.Security.Cryptography;
+
+Console.WriteLine("Ile wygenerować losowań?");
 int losowanie = int.Parse(Console.ReadLine());
 Console.WriteLine("Zestaw wylosowanych liczb:");
 Random random = new Random();
 
-for (int i = 1; i <= losowanie; i++)
+int[] wystapienia = new int[50];
+
+for (int i = 0; i < losowanie; i++)
 {
-    Console.WriteLine("Losowanie " + i + ":");
+    Console.Write("\nLosowanie " + i + ": ");
     for (int j = 0; j < 6; j++)
-        Console.Write(random.Next(1, 49) + ", ");
+    {
+        int liczba = random.Next(1, 49);
+        wystapienia[liczba]++;
+        Console.Write(liczba + (j == 5 ? "" : ", "));
+    }
+    Console.WriteLine();
 }
+
+for (int i = 1; i <= 49; i++)
+{
+    Console.WriteLine($"Wystąpienia liczby {i}: {wystapienia[i]}");
+}
+
+/*
+ * brakuje funkcji do losowania(nie mzoe byc zliczania w tej funkcji);
+ * powt;
+ */
